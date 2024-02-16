6dcf5cb0e70f2b1ceaf82fdd29b74bde7521fe19
Adrien Jones <tjones69@memphis.edu>
2024-02-16 13:03:48 -0600

    Completed code for prob1, prob2, prob3, and prob4

--- prob2.py
+++ prob2.py
@@ -1,4 +1,4 @@
 numbers = [1, 2, 3, 4, 5, 6]
 # Write a lambda function and use filter
-even_numbers = list(filter(lambda x: _, numbers))
+even_numbers = list(filter(lambda x: x if x % 2 == 0 else None, numbers))
 print(even_numbers)
